ab8a8e58174664a8c9ae550e69d2032ced5fc9bf
Polina Miftakhudinova <rambeeech@gmail.com>
2022-01-25 22:02:39 +0400

    Trying to figure out what columns shuold be in user

--- db/migrate/20220125171923_remove_columns_from_user.rb
+++ db/migrate/20220125171923_remove_columns_from_user.rb
@@ -1,7 +1,6 @@
 class RemoveColumnsFromUser < ActiveRecord::Migration[6.1]
   def change
-    remove_column :users, :name
-    remove_column :users, :strategy
-    remove_column :users, :uid
+    remove_column :users, :strategy, :string
+    remove_column :users, :uid, :string
   end
 end

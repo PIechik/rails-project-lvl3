d9f47c73defce9beef9bd666501a93da7496a394
Polina Miftakhudinova <rambeeech@gmail.com>
2022-05-19 17:11:32 +0400

    Fixing rubocop offenses

--- config/initializers/github.rb
+++ config/initializers/github.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: 'user:email, read:user'
+  provider :github, ENV.fetch('GITHUB_KEY', nil), ENV.fetch('GITHUB_SECRET', nil), scope: 'user:email, read:user'
 end
